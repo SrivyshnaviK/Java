663475d73a5f6ba4078a71f0e222a5927c87048a
SrivyshnaviK <srivyshnavi.koduri@zemosolabs.com>
2021-06-14 14:02:23 +0530

    Assignment1

---
+++ Java/Assignment1.java
@@ -0,0 +1,32 @@
+package com.Vyshnavi.assignments;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class SearchFiles {
+    public void findFiles(String path, String regExp) {
+        File directory = new File(path);
+        File fileNames[] = directory.listFiles(); // listing all the fileNames in given directory path
+        if (fileNames!= null && fileNames.length > 0) {
+            for (File file : fileNames)
+                if (file.isDirectory()) { // If file is a Directory to check in the directory
+                    findFiles(file.getAbsolutePath(), regExp);
+                } else if (file.getName().contains(regExp)){
+                        System.out.println(file.getAbsolutePath() + "     " + file.getName());
+                }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("Enter Regular Expression:");
+            String regExp = br.readLine();
+            if(regExp.contentEquals(""))
+                break;
+            SearchFiles searchFiles = new SearchFiles();
+            searchFiles.findFiles("/home", regExp);
+        }
+        System.out.println("File Search Done!!");
+    }
+}
