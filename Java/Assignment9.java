adc3df6ed35f7a73c6f8c01a9de46cad45f302f1
SrivyshnaviK <srivyshnavi.koduri@zemosolabs.com>
2021-06-16 14:53:31 +0530

    Assignment9

---
+++ Java/Assignment9.java
@@ -0,0 +1,23 @@
+package com.Vyshnavi.assignments;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class RegularExpression{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Sentence:");
+        String input = sc.nextLine();
+        String regex = "^[A-Z][A-Za-z0-9\\s]*[.]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()){
+            System.out.println("Sentence begin with Capital and ends with Period.");
+        }
+        else{
+            System.out.println("Sentence either does not begin with Capital or end with Period.");
+        }
+    }
+}
+
